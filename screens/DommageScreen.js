2ea0e8958b48fa3c17c33f0cca488a822753e04c
shelley cavaness <shelleycavaness@yahoo.com>
2020-07-07 12:02:13 +0200

    renamed files congrats and too bad, deleted for cleanup

--- screens/DommageScreen.js
+++ screens/DommageScreen.js
@@ -1,42 +1,92 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button, Image } from 'react-native'
+            
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  Alert,
+  ScrollView,
+} from 'react-native';
 import { dommage, velo } from "../assets";
-// "../assets/images/dommage.jpeg"
 
-export default function DommageScreen ({ navigation }) {
-  console.log('####################dommage', dommage)
+export default function DommageScreen({ navigation }) {
+
     return (
+      <ScrollView style={styles.scrollContainer}>
         <View style={styles.container}>
-           <View style={styles.contentContainer}>
-            <Text>Dommage ! rdv demain</Text>
-           
-           
-          <Image
-            source={{uri :dommage }}
-            style={styles.welcomeImage2}
-           />
-   
-           <Button title="Go back" onPress={() => navigation.goBack()} />
-           </View>   
-           
+          <Image style={styles.photo} source={{uri: dommage}}/>
+          <Text style={styles.companyName}>Dommage !!!</Text>
+          <Text style={styles.slogan}>Rendez-vous demain!</Text>
+          <View style={styles.descriptionContent}>
+            <Text style={styles.description}>
+              Lorem ipsum dolor sit amet, altera conceptam ei cum. Hinc temporibus repudiandae eu mel, cum impetus legendos ei. 
+              Fugit everti dissentias duo cu, nihil fabellas id pri, nonumy verear ea pri. Sit et nisl eros. Ad sapientem forensibus est, 
+              ne vis sonet iuvaret, his sint fabulas dolores ad. Repudiare gubergren voluptatum ius ne, nec nostro possim nostrud ad, 
+            </Text>
+          </View>
+          <TouchableHighlight style={[styles.buttonContainer, styles.sendButton]} onPress={() => navigation.goBack()}>
+            <Text style={styles.buttonText}>Goback</Text>
+          </TouchableHighlight>
         </View>
-    )
+      </ScrollView>
+    );
+  
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-         backgroundColor: '#e3e3e8',     
-    },
-     contentContainer: {
-            padding: 30,
-            alignItems: 'center',
-        },
-    welcomeImage2: {
-    
-      width: 250,
-      height: 250,
-    }    
-})
-
+  scrollContainer:{
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#e0ec80',
+  },
+  photo:{
+    width:200,
+    height:200,
+    justifyContent: 'center',
+    marginBottom:10,
+    marginTop:30,
+    borderRadius: 5,
+    borderWidth: 4,
+    borderColor: "white",
+  },
+  companyName: {
+    fontSize:32,
+    fontWeight: '600',
+    color: '#FFFFFF',
+  },
+  slogan:{
+    fontSize:18,
+    fontWeight: '600',
+    color: '#228B22',
+    marginTop:10,
+  },
+  descriptionContent:{
+    padding:30
+  },
+  description:{
+    fontSize:18,
+    textAlign:'center',
+    marginTop:10,
+    color: '#FFFFFF',
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:100,
+    borderRadius:30,
+  },
+  sendButton: {
+    backgroundColor: "#FFFFFF",
+  },
+  buttonText: {
+    color: '#EE82EE',
+  }
+}); 

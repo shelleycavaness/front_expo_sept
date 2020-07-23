9d769d4893f80da766ee5d9ed0855f5942f5b52c
shelley cavaness <shelleycavaness@yahoo.com>
2020-07-23 07:41:31 +0200

    worked on css for the detailScreen

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -14,27 +14,54 @@ export default function({ route }) {
   const { itemId } = route.params;
 
   const detailTtile =detail[itemId].title
+  const detailDone =detail[itemId].isDone
   const detailDescription =detail[itemId].description
   const detailPhoto = detail[itemId].photo
   console.log('itemId : detail ---------------', itemId)
-// console.log('detail/////////////////', detail[itemId])
-// console.log('detail/////////////////', detail[itemId].id)
-// console.log('detalPhoto', allImages[detailPhoto])
+
 
 // let result = actions.findIndex(x => x.id === 5);
 // console.log('==========================id', result)
   return(
     <View style={styles.container}>
-      <Text>Details Screen</Text>
-      <Text>ItemId: {JSON.stringify(itemId)}</Text>
-      <Text> {detailTtile } </Text>
-      <Text> { detailDescription} </Text>
-      <Image style={styles.image} 
+     <View style={styles.card}>
+
+        <View style={styles.cardHeader}>
+         <View>
+          <Text style={styles.title}>{detailTtile}</Text>
+          <Text style={styles.time}>{detailDone}</Text>
+         </View>
+       </View>
+       <Image style={styles.image} 
         source={allImages[detailPhoto]}
         // source={'../assets/images/abeille.jpeg'}
       />
-        {/* <Text>ItemId: {JSON.stringify(itemId)}</Text>
-        <Text>Title: {JSON.stringify(title)}</Text> */}
+       <View style={styles.separator}/>
+       <View style={styles.footer}>
+         <View style={styles.contentContainer}>   
+            <View style={styles.contentSection}>         
+              <Text style={styles.contentSectionText}>{ detailTtile }</Text>       
+            </View>    
+          
+            <View style={styles.contentSection}>         
+              <Text style={styles.contentSectionText}>{ detailTtile }</Text>       
+            </View>  
+            <View style={styles.contentSection}>         
+              <Text style={styles.contentSectionText}>{ detailTtile }</Text>       
+            </View>  
+            <View style={styles.contentSection}>         
+              <Text style={styles.contentSectionText}>{ detailDescription }</Text>       
+            </View>       
+          </View> 
+          <View style={styles.contentContainer}>  
+          <View style={styles.contentSection}>         
+              <Text style={styles.contentSectionText}>{ detailDescription }</Text>       
+            </View>  
+          </View> 
+ 
+       </View> 
+      
+     </View> 
     </View>
   )
 }
@@ -42,11 +69,79 @@ export default function({ route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
+    // marginTop:1,
+    paddingHorizontal: 17,
+    backgroundColor:"#1E6E6",
+
     alignItems: 'center', 
-    justifyContent: 'center'
+    // justifyContent: 'center'
+    paddingTop: 10
+  },
+  card:{
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 2,
+      
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    marginVertical: 8,
+    // backgroundColor:"#E6E6",
+    backgroundColor:"white",
+
+  },
+  cardHeader: {
+    paddingVertical: 20,
+    paddingHorizontal: 16,
+    borderTopLeftRadius: 1,
+    borderTopRightRadius: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   image:{
-    width: 60,
-    height: 60,
-  }
+    // width: 200,
+    height: 300,
+    flex: 1,
+  },
+  separator: {
+    marginTop: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderStyle: 'solid',
+  },
+  footer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 12.5,
+    paddingBottom: 25,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 1,
+    borderBottomRightRadius: 1,
+    borderColor: 'orange',
+    borderWidth: 1,
+    borderStyle: 'solid',
+  },
+  contentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    // flexDirection: 'row',
+    flex: 1,
+    borderColor: 'black',
+       borderWidth: 5,
+       borderStyle: 'solid',  
+  }, 
+  contentSection: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flex: 1,
+    borderColor: 'yellow',
+       borderWidth: 5,
+       borderStyle: 'solid',
+  },
+  contentSectionText: {
+    marginLeft: 8,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+  },
 })
+
